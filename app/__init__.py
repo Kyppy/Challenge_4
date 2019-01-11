c6c021049d0c380af333b4b6aaead4d300999c63
Kyppy Simani <simanikyppy@gmail.com>
2019-01-11 21:09:32 +0300

    #163130412 Consume 'Sign-up' resource

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask, Blueprint
 from flask_jwt_extended import(JWTManager, jwt_required, create_access_token)
 from .api.v1 import version_one as v1
+from flask_cors import CORS
 import os
 
 
 def create_app():
     app = Flask(__name__)
-    # app.config.from_object(config[config_name])
+    CORS(app)
     app.register_blueprint(v1)
     app.config['JWT_SECRET_KEY'] = os.getenv('SECRET')
     jwt = JWTManager(app)    
